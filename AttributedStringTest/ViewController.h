dc11b9f6fdd06055a13b004ad0ce41f0caada49b
Ben Shive <ben.shive@gmail.com>
2014-10-08 21:29:36 -0400

    Initial Commit

---
+++ AttributedStringTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttributedStringTest
+//
+//  Created by Benjamin Shive on 10/8/14.
+//  Copyright (c) 2014 Philadelphia Code Works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AttributedStringTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AttributedStringTest
+//
+//  Created by Benjamin Shive on 10/8/14.
+//  Copyright (c) 2014 Philadelphia Code Works. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
